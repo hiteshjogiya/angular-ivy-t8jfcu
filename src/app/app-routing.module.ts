86c69104116c4e752d148728588c1ed7fc320bed
hiteshjogiya <hiteshjogiya2017@gmail.com>
2021-04-22 21:02:29 +0530

    new routing file added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,17 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DatatableComponent } from './datatable/datatable.component';
-import { ErrorNotFoundComponentComponent } from './error-not-found-component/error-not-found-component.component';
-import { HomeComponent } from './home/home.component';
-import { LiveapiComponent } from './liveapi/liveapi.component';
-import { ReactivesignupComponent } from './reactivesignup/reactivesignup.component';
-import { ResultComponent } from './result/result.component';
-import { SelectstudentComponent } from './selectstudent/selectstudent.component';
+import { AuthGuard } from './auth.guard';
+import { BookComponent } from './book/book.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { SignupComponent } from './signup/signup.component';
-import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
-  {path:"studentlist",component:StudentComponent},
-  {path:"resultlist",component:ResultComponent},
-  {path:"home",component:HomeComponent},
-  {path:"selectstudent",component:SelectstudentComponent},
-  {path:"selectstudent/:id",component:SelectstudentComponent},
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:"api",component:LiveapiComponent},
+  {path:"",redirectTo:"/login",pathMatch:"full"},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"book",component:BookComponent,canActivate:[AuthGuard]},
   {path:"up" ,component:SignupComponent},
-  {path:"rsign",component:ReactivesignupComponent},
-  {path:"datatable",component:DatatableComponent},
-  {path:"**",component:ErrorNotFoundComponentComponent}
-  
- 
-
 ];
 
 @NgModule({
